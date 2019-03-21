99858197effba84a0d66ae21ecdb5ae1f8890955
ekkacipta <ekkacipta@yahoo.com>
2019-03-21 20:50:13 +0700

    add dotenv and firebasejs

--- client/src/api/firebase.js
+++ client/src/api/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase";
 
 var app = initializeApp({
-    apiKey: "AIzaSyAj8TCcNMWrghgYi_G6q0QMzk1eq86t0m8",
-    authDomain: "atheque-6dbb6.firebaseapp.com",
-    databaseURL: "https://atheque-6dbb6.firebaseio.com",
-    projectId: "atheque-6dbb6",
-    storageBucket: "atheque-6dbb6.appspot.com",
-    messagingSenderId: "101334149001"
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId
   });
   
 const db = app.firestore()
