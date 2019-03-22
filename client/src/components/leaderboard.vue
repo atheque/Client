e3538e6d300dd6cbc2cfa4413f05f16e2a123831
ekkacipta <ekkacipta@yahoo.com>
2019-03-22 12:53:05 +0700

    ad new leaderboard component

---
+++ client/src/components/leaderboard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="modal fade" id="basicExampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"><i class="fas fa-trophy"></i> Leaderboard</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+
+                <div class="card-body">
+                <ul class="list-group">
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    1.Cras justo odio
+                    <span class="badge badge-primary badge-pill">14</span>
+                </li>
+                </ul>
+            </div>
+                
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+            </div>
+        </div>
+    </div>
+</template>
